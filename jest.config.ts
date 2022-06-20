5b72134c4e13e8a7386332c9316f488fa025241d
Rohan Mitchell <rohan@rohanmitchell.com>
2022-06-21 09:29:26 +1000

    Install ts-node and configure jest

---
+++ jest.config.ts
@@ -0,0 +1,12 @@
+/*
+ * For a detailed explanation regarding each configuration property and type check, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+export default {
+  // Automatically clear mock calls, instances, contexts and results before every test
+  clearMocks: true,
+
+  // The root directory that Jest should scan for tests and modules within
+  rootDir: "src",
+};
